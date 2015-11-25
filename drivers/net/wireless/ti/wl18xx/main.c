b6d2a3290137a44faad51745dc1a6d120f5029df
Maital Hahn <maitalm@ti.com>
2015-11-25 17:25:37 +0200

    mesh: Mesh Point always asserts "HT Mixed Mode" protection [WORK-AROUND]
    There are 3 diffrences between an AP beacon and a mesh beacon, all of
    them are in the HT capabilities and HT information IEs.
    These 2 IEs are also diffrent between the mesh beacon and the mesh action frames.
    These diffrences results from the fact that the AP beacon and the mesh action frames
    are both being set/built by the hostap package, while the mesh beacon is being built
    by the mac80211 layer in the kernel.
    
    In order to align the mesh beacon to the action frames (and to the AP beacon),
    we changed the default parameters in the kernel to metch the hostap defaults.
    
    (The correct fix will be to set these IEs from the hostap also for
    the mesh beacons, as it is done for the AP beacon).
    
    fixes #WILINK8_SW_DEV-69
    
    Signed-off-by: Maital Hahn <maitalm@ti.com>
    Acked-by: Yaniv Machani <yanivma@ti.com>

--- drivers/net/wireless/ti/wl18xx/main.c
+++ drivers/net/wireless/ti/wl18xx/main.c
@@ -1749,11 +1749,12 @@ static struct wlcore_ops wl18xx_ops = {
 	.dfs_master_restart	= wl18xx_cmd_dfs_master_restart,
 };
 
+/** Setting IEEE80211_HT_CAP_SM_PS in the ht_cap means to disable the SMPS **/
 /* HT cap appropriate for wide channels in 2Ghz */
 static struct ieee80211_sta_ht_cap wl18xx_siso40_ht_cap_2ghz = {
 	.cap = IEEE80211_HT_CAP_SGI_20 | IEEE80211_HT_CAP_SGI_40 |
 	       IEEE80211_HT_CAP_SUP_WIDTH_20_40 | IEEE80211_HT_CAP_DSSSCCK40 |
-	       IEEE80211_HT_CAP_GRN_FLD,
+	       IEEE80211_HT_CAP_GRN_FLD | IEEE80211_HT_CAP_SM_PS,
 	.ht_supported = true,
 	.ampdu_factor = IEEE80211_HT_MAX_AMPDU_16K,
 	.ampdu_density = IEEE80211_HT_MPDU_DENSITY_16,
@@ -1768,7 +1769,7 @@ static struct ieee80211_sta_ht_cap wl18xx_siso40_ht_cap_2ghz = {
 static struct ieee80211_sta_ht_cap wl18xx_siso40_ht_cap_5ghz = {
 	.cap = IEEE80211_HT_CAP_SGI_20 | IEEE80211_HT_CAP_SGI_40 |
 	       IEEE80211_HT_CAP_SUP_WIDTH_20_40 |
-	       IEEE80211_HT_CAP_GRN_FLD,
+	       IEEE80211_HT_CAP_GRN_FLD | IEEE80211_HT_CAP_SM_PS,
 	.ht_supported = true,
 	.ampdu_factor = IEEE80211_HT_MAX_AMPDU_16K,
 	.ampdu_density = IEEE80211_HT_MPDU_DENSITY_16,
@@ -1782,7 +1783,7 @@ static struct ieee80211_sta_ht_cap wl18xx_siso40_ht_cap_5ghz = {
 /* HT cap appropriate for SISO 20 */
 static struct ieee80211_sta_ht_cap wl18xx_siso20_ht_cap = {
 	.cap = IEEE80211_HT_CAP_SGI_20 |
-	       IEEE80211_HT_CAP_GRN_FLD,
+	       IEEE80211_HT_CAP_GRN_FLD | IEEE80211_HT_CAP_SM_PS,
 	.ht_supported = true,
 	.ampdu_factor = IEEE80211_HT_MAX_AMPDU_16K,
 	.ampdu_density = IEEE80211_HT_MPDU_DENSITY_16,
@@ -1796,7 +1797,7 @@ static struct ieee80211_sta_ht_cap wl18xx_siso20_ht_cap = {
 /* HT cap appropriate for MIMO rates in 20mhz channel */
 static struct ieee80211_sta_ht_cap wl18xx_mimo_ht_cap_2ghz = {
 	.cap = IEEE80211_HT_CAP_SGI_20 |
-	       IEEE80211_HT_CAP_GRN_FLD,
+	       IEEE80211_HT_CAP_GRN_FLD | IEEE80211_HT_CAP_SM_PS,
 	.ht_supported = true,
 	.ampdu_factor = IEEE80211_HT_MAX_AMPDU_16K,
 	.ampdu_density = IEEE80211_HT_MPDU_DENSITY_16,
